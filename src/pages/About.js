1e36746423a2a39d38d94643c3d696b063c84326
Shompoth KHAN <dadoulan@gmail.com>
2022-01-01 22:34:34 +0100

    created pages, routes, navbar, footer

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl">About</h1>
+            <p className="mb-4 text-2xl font-light">
+                A React App to search Github profiles and see profile details.
+            </p>
+        </div>
+    );
+}
+
+export default About;
